f2667dbf9a1dae34f0c0d12ae1f4c941421b27f4
infinite-dev22 <mwigojm@gmail.com>
2024-08-24 01:32:16 +0300

    Add text area

---
+++ lib/global/presentation/widget/text_fields/my_text_area.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/app_colors.dart';
+
+class MyTextArea extends StatelessWidget {
+  final String? hint;
+  final bool disabled;
+  final TextEditingController? controller;
+
+  const MyTextArea({
+    super.key,
+    this.hint,
+    this.disabled = false,
+    this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      padding: const EdgeInsets.only(bottom: 10),
+      child: TextFormField(
+        keyboardType: TextInputType.multiline,
+        minLines: 4,
+        maxLines: 6,
+        enabled: !disabled,
+        maxLength: 200,
+        controller: controller,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(
+            vertical: 5.0,
+            horizontal: 10.0,
+          ),
+          hintText: hint,
+          hintStyle: const TextStyle(color: secondary),
+          enabled: !disabled,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(
+              color: secondaryLightest,
+            ),
+          ),
+          disabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(
+              color: secondaryLightest,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(
+              color: secondaryLightest,
+            ),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(
+              color: secondaryLightest,
+            ),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(
+              color: secondaryLightest,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(
+              color: secondaryLightest,
+            ),
+          ),
+          fillColor: secondaryLightest,
+          filled: true,
+          isDense: true,
+          counter: null,
+          counterText: "",
+        ),
+      ),
+    );
+  }
+}
