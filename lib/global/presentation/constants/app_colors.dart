bbc918306539da14f2035174073c51ed3bad03e7
infinite-dev22 <mwigojm@gmail.com>
2024-08-23 21:36:34 +0300

    Add app colors initial

---
+++ lib/global/presentation/constants/app_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+const Color primary = Color(0xff08967E);
+const Color secondaryLightest = Color(0xffF9F9F9);
+const Color secondaryLight = Color(0xffeff8f5);
+const Color secondary = Color(0xffACB3BC);
+const Color secondaryDark = Color(0xff22262E);
