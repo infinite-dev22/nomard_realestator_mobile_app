e49a8ddbd424953328a74776c0332e44d9cb761f
infinite-dev22 <mwigojm@gmail.com>
2024-08-23 21:36:42 +0300

    Add app styles initial

---
+++ lib/global/presentation/constants/styles.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle heading1Style = GoogleFonts.roboto(
+  fontSize: 34,
+  fontWeight: FontWeight.w400,
+);
+TextStyle heading2Style = GoogleFonts.roboto(
+  fontSize: 28,
+  fontWeight: FontWeight.w600,
+);
+TextStyle heading3Style = GoogleFonts.roboto(
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+TextStyle subHeadingStyle = GoogleFonts.roboto(
+  fontSize: 20,
+  fontWeight: FontWeight.w400,
+);
+TextStyle headingLineStyle = GoogleFonts.roboto(
+  fontSize: 30,
+  fontWeight: FontWeight.w700,
+);
+TextStyle bodyStyle = GoogleFonts.roboto(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+);
+TextStyle captionStyle = GoogleFonts.roboto(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+);
