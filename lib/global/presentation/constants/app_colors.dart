932f33c46b5d4050b552d1cab7c796b15028473c
infinite-dev22 <mwigojm@gmail.com>
2024-08-23 22:42:46 +0300

    Add more colors

--- lib/global/presentation/constants/app_colors.dart
+++ lib/global/presentation/constants/app_colors.dart
@@ -5,3 +5,5 @@ const Color secondaryLightest = Color(0xffF9F9F9);
 const Color secondaryLight = Color(0xffeff8f5);
 const Color secondary = Color(0xffACB3BC);
 const Color secondaryDark = Color(0xff22262E);
+const Color bgDark = Color(0xff22262E);
+const Color bgLight = Color(0x0fffffff);
