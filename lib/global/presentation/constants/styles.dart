7020f7945fd71689cc1e80bc51e4bfe7c6b75c21
infinite-dev22 <mwigojm@gmail.com>
2024-08-23 22:34:27 +0300

    Remove unnecessary text styles

--- lib/global/presentation/constants/styles.dart
+++ lib/global/presentation/constants/styles.dart
@@ -1,27 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-TextStyle heading1Style = GoogleFonts.roboto(
-  fontSize: 34,
-  fontWeight: FontWeight.w400,
-);
-TextStyle heading2Style = GoogleFonts.roboto(
-  fontSize: 28,
-  fontWeight: FontWeight.w600,
-);
-TextStyle heading3Style = GoogleFonts.roboto(
-  fontSize: 24,
-  fontWeight: FontWeight.w600,
-);
-TextStyle subHeadingStyle = GoogleFonts.roboto(
+TextStyle boldStyle = GoogleFonts.roboto(
   fontSize: 20,
+  fontWeight: FontWeight.w600,
+);
+TextStyle semiBoldStyle = GoogleFonts.roboto(
+  fontSize: 18,
   fontWeight: FontWeight.w400,
 );
-TextStyle headingLineStyle = GoogleFonts.roboto(
-  fontSize: 30,
-  fontWeight: FontWeight.w700,
-);
-TextStyle bodyStyle = GoogleFonts.roboto(
+TextStyle regularStyle = GoogleFonts.roboto(
   fontSize: 16,
   fontWeight: FontWeight.w400,
 );
--- lib/global/presentation/widget/texts/my_text.dart
+++ lib/global/presentation/widget/texts/my_text.dart
@@ -5,17 +5,21 @@ import 'package:real_estate_property/global/presentation/constants/styles.dart';
 class MyText extends StatelessWidget {
   final String text;
   final TextStyle style;
-  MyText.h1(this.text, {super.key}) : style = heading1Style;
-  MyText.h2(this.text, {super.key}) : style = heading2Style;
-  MyText.h3(this.text, {super.key}) : style = heading3Style;
-  MyText.headline(this.text, {super.key}) : style = headingLineStyle;
-  MyText.subHeading(this.text, {super.key}) : style = subHeadingStyle;
+
+  MyText.bold(this.text, {super.key}) : style = boldStyle;
+
+  MyText.semiBold(this.text, {super.key}) : style = semiBoldStyle;
+
   MyText.caption(this.text, {super.key}) : style = captionStyle;
 
-  MyText.body(this.text, {super.key, Color color = secondaryDark}) : style = bodyStyle.copyWith(color: color);
+  MyText.regular(this.text, {super.key, Color color = secondaryDark})
+      : style = regularStyle.copyWith(color: color);
 
   @override
   Widget build(BuildContext context) {
-    return Text(text, style: style,);
+    return Text(
+      text,
+      style: style,
+    );
   }
 }
