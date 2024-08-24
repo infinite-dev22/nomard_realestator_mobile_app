07649d0e4c0dc697c57bedcd82f588c0c8bfd487
infinite-dev22 <mwigojm@gmail.com>
2024-08-24 19:37:49 +0300

    Add agent item

---
+++ lib/property_view/presentation/pages/about/data/model/agent_model.dart
@@ -0,0 +1,7 @@
+class AgentModel {
+  final String image;
+  final String name;
+  final String type;
+
+  AgentModel(this.image, this.name, this.type);
+}
---
+++ lib/property_view/presentation/pages/about/presentation/widget/agent_item.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:real_estate_property/property_view/presentation/pages/about/data/model/agent_model.dart';
+
+import '../../../../../../global/presentation/constants/app_colors.dart';
+import '../../../../../../global/presentation/widget/images/user_profile_image.dart';
+import '../../../../../../global/presentation/widget/texts/my_text.dart';
+
+class AgentItem extends StatelessWidget {
+  final AgentModel agent;
+
+  const AgentItem(this.agent, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            UserProfileImage(agent.image),
+            const SizedBox(width: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                MyText.bold(agent.name),
+                const SizedBox(width: 5),
+                MyText.caption(
+                  agent.type,
+                  color: secondary,
+                ),
+              ],
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.chat_outlined),
+              color: secondaryDark,
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(
+                  secondaryLight,
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.phone_outlined),
+              color: secondaryDark,
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(
+                  secondaryLight,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
