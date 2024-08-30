2c6fd8cc402bd1b2e905aa8a7f3aa3a3bc0d76ea
infinite-dev22 <mwigojm@gmail.com>
2024-08-31 02:51:55 +0300

    Add Featured Page
    This commit introduces a new Featured page to the application. It includes:
    
    - A FeaturedLayout widget that displays a list of featured properties in
     a grid layout.
    - A search bar and settings icon in the app bar.
    - A badge on the notifications icon to indicate unread notifications.

---
+++ lib/home/presentation/pages/featured/presentation/pages/featured.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../widget/featured_layout.dart';
+
+class Featured extends StatelessWidget {
+  const Featured({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: FeaturedLayout());
+  }
+}
