8f1b05e86038410ff8c6171cf4e6b2007de7f5ad
infinite-dev22 <mwigojm@gmail.com>
2024-08-31 02:50:16 +0300

    Add Review Page
    This commit introduces a new Review Page with a basic layout. It includes:
    
    - A new Review widget that serves as the entry point for
     the page.
    - A ReviewLayout widget that provides the basic structure and displays a placeholder text.

---
+++ lib/property_view/presentation/pages/reviews/presentation/page/review.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:real_estate_property/property_view/presentation/pages/reviews/presentation/widget/review_layout.dart';
+
+class Review extends StatelessWidget {
+  const Review({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ReviewLayout();
+  }
+}
---
+++ lib/property_view/presentation/pages/reviews/presentation/widget/review_layout.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ReviewLayout extends StatelessWidget {
+  const ReviewLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Review Page"),
+    );
+  }
+}
