ec4bb6bb22795a723df7e053a9059dd7882946ca
infinite-dev22 <mwigojm@gmail.com>
2024-09-04 02:31:39 +0300

    Rename ReviewItem to ReviewSummaryItem
    Renamed the ReviewItem widget to ReviewSummaryItem and moved it to the review_summary folder. Also, updated the
     import paths accordingly.

--- lib/payment/presentation/pages/review_summary/presentation/widget/review_summary_item.dart
+++ lib/payment/presentation/pages/review_summary/presentation/widget/review_summary_item.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:real_estate_property/payment/presentation/pages/review/presentation/widget/review_property_details.dart';
+import 'package:real_estate_property/payment/presentation/pages/review_summary/presentation/widget/review_summary_property_details.dart';
 import 'package:real_estate_property/property_view/presentation/pages/gallery/presentation/widget/images/small_property_image.dart';
 
 import '../../../../../../global/data/model/property_model.dart';
 
-class ReviewItem extends StatelessWidget {
+class ReviewSummaryItem extends StatelessWidget {
   final PropertyModel propertyModel;
 
-  const ReviewItem(this.propertyModel, {super.key});
+  const ReviewSummaryItem(this.propertyModel, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +20,7 @@ class ReviewItem extends StatelessWidget {
         children: [
           SmallPropertyImage(propertyModel.images[1]),
           const SizedBox(width: 20),
-          ReviewPropertyDetails(
+          ReviewSummaryPropertyDetails(
             propertyModel,
           ),
         ],
