30d6d3c0aa156f3b4eef12c2dd54715fde3a8361
infinite-dev22 <mwigojm@gmail.com>
2024-09-05 02:09:10 +0300

    Add OTP page
    Added a new OTP page with a layout for entering and verifying OTP.
    The page includes a resend code timer and a verify button.

--- lib/authentication/presentation/pages/sign_in/presentation/widget/otp_layout.dart
+++ lib/authentication/presentation/pages/sign_in/presentation/widget/otp_layout.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:real_estate_property/global/presentation/constants/app_colors.dart';
 import 'package:real_estate_property/global/presentation/widget/buttons/my_button.dart';
-import 'package:real_estate_property/global/presentation/widget/icon_holders/my_round_icon.dart';
 import 'package:real_estate_property/global/presentation/widget/text_fields/my_otp_field.dart';
 import 'package:real_estate_property/global/presentation/widget/texts/my_text.dart';
 
@@ -10,69 +10,42 @@ class OTPLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: LayoutBuilder(builder: (context, constraints) {
-        return Column(
-          children: [
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                MyRoundIcon.small(
-                  icon: Icons.arrow_back,
-                  elevated: true,
-                ),
-              ],
-            ),
-            MyText.h1("Enter Your OTP"),
-            const SizedBox(height: 16),
-            SizedBox(
-              width: constraints.maxWidth * .6,
-              child: MyText.regular(
-                "Lorem ipsum is simply dummy text ot the printing and typesetting",
-                color: secondary,
-                softWrap: true,
-              ),
-            ),
-            const SizedBox(height: 30),
-            Column(
-              children: [
-                const MyOtpField(),
-                const SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    MyText.regular(
-                      "Resend Code In",
-                      color: secondaryDark,
-                      softWrap: true,
-                    ),
-                    const SizedBox(width: 5),
-                    MyText.regular(
-                      "20",
-                      color: Theme.of(context).colorScheme.primary,
-                      softWrap: true,
-                    ),
-                    const SizedBox(width: 5),
-                    MyText.regular(
-                      "Sec",
-                      color: secondaryDark,
-                      softWrap: true,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 16),
-                MyButton(
-                  text: "Verify",
-                  onTap: () {},
-                ),
-              ],
-            ),
-          ],
-        );
-      }),
+    var _widgets = <Widget>[
+      const MyOtpField(),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          MyText.regular(
+            "Resend Code In",
+            color: secondaryDark,
+            softWrap: true,
+          ),
+          const SizedBox(width: 5),
+          MyText.regular(
+            "20",
+            color: Theme.of(context).colorScheme.primary,
+            softWrap: true,
+          ),
+          const SizedBox(width: 5),
+          MyText.regular(
+            "Sec",
+            color: secondaryDark,
+            softWrap: true,
+          ),
+        ],
+      ),
+      MyButton(
+        text: "Verify",
+        onTap: () => GoRouter.of(context).pushNamed("reset"),
+      ),
+    ];
+
+    return ListView.separated(
+      padding: const EdgeInsets.only(top: 30, left: 16, right: 16),
+      itemCount: _widgets.length,
+      itemBuilder: (context, index) => _widgets[index],
+      separatorBuilder: (BuildContext context, int index) =>
+      const SizedBox(height: 24),
     );
   }
 }
-
-class MyCheckbox {}
