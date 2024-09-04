10cadf52a211d4cb7901a99a4ccd4df9122dbc6e
infinite-dev22 <mwigojm@gmail.com>
2024-09-04 14:54:44 +0300

    Add onTap callback to ChatItem
    This commit adds an optional onTap callback to the ChatItem widget, allowing for custom actions to be performed when the item
     is tapped.

--- lib/chat_room/presentation/widget/chat_item.dart
+++ lib/chat_room/presentation/widget/chat_item.dart
@@ -7,57 +7,66 @@ import '../../data/model/chat_item_model.dart';
 
 class ChatItem extends StatelessWidget {
   final ChatItemModel chatItem;
+  final Function()? onTap;
 
-  const ChatItem(this.chatItem, {super.key});
+  const ChatItem(
+    this.chatItem, {
+    super.key,
+    this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
-      return Row(
-        children: [
-          UserProfileImage(chatItem.image),
-          const SizedBox(width: 10),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    MyText.bold(chatItem.name),
-                    if (chatItem.messageCount! > 0)
-                      Badge(
-                        label: MyText.caption(
-                          chatItem.messageCount.toString(),
-                          color: Colors.white,
+      return GestureDetector(
+        onTap: onTap,
+        child: Row(
+          children: [
+            UserProfileImage(chatItem.image),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      MyText.bold(chatItem.name),
+                      if (chatItem.messageCount! > 0)
+                        Badge(
+                          label: MyText.caption(
+                            chatItem.messageCount.toString(),
+                            color: Colors.white,
+                          ),
+                          backgroundColor:
+                              Theme.of(context).colorScheme.primary,
                         ),
-                        backgroundColor: Theme.of(context).colorScheme.primary,
+                    ],
+                  ),
+                  const SizedBox(width: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: constraints.maxWidth - 100,
+                        child: MyText.caption(
+                          chatItem.messagePreview,
+                          color: secondary,
+                          overflow: TextOverflow.ellipsis,
+                        ),
                       ),
-                  ],
-                ),
-                const SizedBox(width: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: constraints.maxWidth - 100,
-                      child: MyText.caption(
-                        chatItem.messagePreview,
+                      MyText.caption(
+                        chatItem.time,
                         color: secondary,
-                        overflow: TextOverflow.ellipsis,
                       ),
-                    ),
-                    MyText.caption(
-                      chatItem.time,
-                      color: secondary,
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       );
     });
   }
