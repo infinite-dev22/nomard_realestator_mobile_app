7a92ce26a212dec1a5aa40bb1a2c992e4305256c
infinite-dev22 <mwigojm@gmail.com>
2024-09-05 02:09:27 +0300

    Feat: Add reset password page
    This commit introduces a new reset password page with basic UI elements.
    It includes a password and confirm password field, along with
     a "Reset Password" button.
    The button currently navigates to the sign-in page upon tapping.

---
+++ lib/authentication/presentation/pages/sign_in/presentation/pages/reset_password.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:real_estate_property/authentication/presentation/pages/sign_in/presentation/widget/reset_password_layout.dart';
+import 'package:real_estate_property/global/presentation/constants/app_colors.dart';
+import 'package:real_estate_property/global/presentation/widget/buttons/my_back_button.dart';
+import 'package:real_estate_property/global/presentation/widget/texts/my_text.dart';
+
+class ResetPassword extends StatelessWidget {
+  const ResetPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        leading: (GoRouter.of(context).canPop() == true)
+            ? MyBackButton(
+                onTap: () => GoRouter.of(context).pop(),
+              )
+            : null,
+        bottom: PreferredSize(
+          preferredSize: const Size(double.infinity, 100),
+          child: Center(
+            child: Column(
+              children: [
+                MyText.h1("Reset Password"),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: size.width * .6,
+                  child: MyText.regular(
+                    "Lorem ipsum is simply dummy text ot the printing and typesetting",
+                    color: secondary,
+                    softWrap: true,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: const ResetPasswordLayout(),
+    );
+  }
+}
--- lib/authentication/presentation/pages/sign_in/presentation/widget/reset_password_layout.dart
+++ lib/authentication/presentation/pages/sign_in/presentation/widget/reset_password_layout.dart
@@ -1,68 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:icons_plus/icons_plus.dart';
-import 'package:real_estate_property/authentication/presentation/pages/sign_in/presentation/widget/sign_in_link.dart';
-import 'package:real_estate_property/authentication/presentation/pages/sign_up/presentation/widget/or_widget.dart';
-import 'package:real_estate_property/global/presentation/constants/app_colors.dart';
 import 'package:real_estate_property/global/presentation/widget/buttons/my_button.dart';
-import 'package:real_estate_property/global/presentation/widget/icon_holders/my_round_icon.dart';
-import 'package:real_estate_property/global/presentation/widget/social_auth_widget.dart';
 import 'package:real_estate_property/global/presentation/widget/text_fields/my_text_field.dart';
-import 'package:real_estate_property/global/presentation/widget/texts/my_text.dart';
-
-import 'forgot_password_link.dart';
 
 class ResetPasswordLayout extends StatelessWidget {
   const ResetPasswordLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: LayoutBuilder(builder: (context, constraints) {
-        return Column(
-          children: [
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                MyRoundIcon.small(
-                  icon: Icons.arrow_back,
-                  elevated: true,
-                ),
-              ],
-            ),
-            MyText.h1("Reset Password"),
-            const SizedBox(height: 16),
-            SizedBox(
-              width: constraints.maxWidth * .6,
-              child: MyText.regular(
-                "Lorem ipsum is simply dummy text ot the printing and typesetting",
-                color: secondary,
-                softWrap: true,
-              ),
-            ),
-            const SizedBox(height: 30),
-            Column(
-              children: [
-                const MyTextField.obsecure(
-                  hint: "Password",
-                  prefixIcon: Icon(MingCute.lock_line),
-                ),
-                const MyTextField.obsecure(
-                  hint: "Confirm Password",
-                  prefixIcon: Icon(MingCute.lock_line),
-                ),
-                const SizedBox(height: 16),
-                MyButton(
-                  text: "Sign In",
-                  onTap: () {},
-                ),
-              ],
-            ),
-          ],
-        );
-      }),
+    var _widgets = <Widget>[
+      const MyTextField.obsecure(
+        hint: "Password",
+        prefixIcon: Icon(MingCute.lock_line),
+      ),
+      const MyTextField.obsecure(
+        hint: "Confirm Password",
+        prefixIcon: Icon(MingCute.lock_line),
+      ),
+      MyButton(
+        text: "Reset Password",
+        onTap: () => GoRouter.of(context).goNamed("sign_in"),
+      ),
+    ];
+
+    return ListView.separated(
+      padding: const EdgeInsets.only(top: 30, left: 16, right: 16),
+      itemCount: _widgets.length,
+      itemBuilder: (context, index) => _widgets[index],
+      separatorBuilder: (BuildContext context, int index) =>
+          const SizedBox(height: 16),
     );
   }
 }
-
-class MyCheckbox {}
