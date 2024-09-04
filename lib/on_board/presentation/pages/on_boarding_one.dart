f2306f1e6c86ae3ed0401d91d325428deb40fd87
infinite-dev22 <mwigojm@gmail.com>
2024-09-04 20:17:59 +0300

    Remove unused imports and replace Home widget with Placeholder
    This commit removes unused imports from the `on_boarding_one.dart` file and replaces the `Home
    ` widget with a `Placeholder` widget. This change simplifies the code and makes it more maintainable.

--- lib/on_board/presentation/pages/on_boarding_one.dart
+++ lib/on_board/presentation/pages/on_boarding_one.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
 
-import '../../../authentication/presentation/pages/sign_in/presentation/pages/sign_in.dart';
-import '../../../authentication/presentation/pages/sign_up/presentation/pages/sign_up.dart';
-import '../../../home/presentation/pages/home.dart';
-import '../../../home/presentation/widget/home_layout.dart';
-
 class OnBoarding extends StatelessWidget {
   const OnBoarding({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Home(),
+    return const Scaffold(
+      body: Placeholder(),
     );
   }
 }
