2acefab8bafa23316885123376698720c851a21c
infinite-dev22 <mwigojm@gmail.com>
2024-09-05 01:23:11 +0300

    Refactor: Restructure Sign In Page Layout
    Moved the title and description from SignInLayout to SignIn page for better organization.
    Adjusted spacing and padding for
     improved visual appeal.
    Removed unnecessary MyCheckbox class.

--- lib/authentication/presentation/pages/sign_in/presentation/pages/sign_in.dart
+++ lib/authentication/presentation/pages/sign_in/presentation/pages/sign_in.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
-
-import '../widget/reset_password_layout.dart';
+import 'package:real_estate_property/authentication/presentation/pages/sign_in/presentation/widget/sign_in_layout.dart';
+import 'package:real_estate_property/global/presentation/constants/app_colors.dart';
+import 'package:real_estate_property/global/presentation/widget/buttons/my_back_button.dart';
+import 'package:real_estate_property/global/presentation/widget/texts/my_text.dart';
 
 class SignIn extends StatelessWidget {
   const SignIn({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: SafeArea(child: ResetPasswordLayout()));
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        leading: MyBackButton(
+          onTap: () {},
+        ),
+        bottom: PreferredSize(
+          preferredSize: const Size(double.infinity, 100),
+          child: Center(
+            child: Column(
+              children: [
+                MyText.h1("Sign In"),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: size.width * .6,
+                  child: MyText.regular(
+                    "Lorem ipsum is simply dummy text ot the printing and typesetting",
+                    color: secondary,
+                    softWrap: true,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: const SignInLayout(),
+    );
   }
 }
