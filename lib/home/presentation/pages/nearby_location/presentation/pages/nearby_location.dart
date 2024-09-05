5056b8ce8cbccbcdc61a4ac9ea909e70b0703dd2
infinite-dev22 <mwigojm@gmail.com>
2024-09-05 05:11:36 +0300

    Refactor: Move search bar to app bar
    Moved the search bar and settings icon from the body of the Nearby Location page to the app bar to improve user
     experience.
    This change provides a more consistent and familiar interface for users to search for nearby locations.

--- lib/home/presentation/pages/nearby_location/presentation/pages/nearby_location.dart
+++ lib/home/presentation/pages/nearby_location/presentation/pages/nearby_location.dart
@@ -1,12 +1,69 @@
 import 'package:flutter/material.dart';
-
-import '../widget/nearby_location_layout.dart';
+import 'package:go_router/go_router.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:real_estate_property/global/presentation/widget/buttons/my_back_button.dart';
+import 'package:real_estate_property/global/presentation/widget/icon_holders/my_round_icon.dart';
+import 'package:real_estate_property/global/presentation/widget/text_fields/my_text_field.dart';
+import 'package:real_estate_property/global/presentation/widget/texts/my_text.dart';
+import 'package:real_estate_property/home/presentation/pages/nearby_location/presentation/widget/nearby_location_layout.dart';
 
 class NearByLocation extends StatelessWidget {
   const NearByLocation({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: NearByLocationLayout());
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          leading: Builder(
+            builder: (context) => MyBackButton(
+              onTap: () => GoRouter.of(context).pop(),
+            ),
+          ),
+          title: MyText.h2("NearBy Location"),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Badge(
+                label: MyText.caption(
+                  "5",
+                  color: Colors.white,
+                ),
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                child: const MyRoundIcon.small(
+                  icon: Icons.notifications_none_rounded,
+                  elevated: true,
+                ),
+              ),
+            ),
+          ],
+          bottom: PreferredSize(
+            preferredSize: const Size(double.infinity, 70),
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * .74,
+                    child: const MyTextField(
+                      hint: "Search",
+                      prefixIcon: Icon(MingCute.search_2_fill),
+                    ),
+                  ),
+                  MyRoundIcon.small(
+                    icon: MingCute.settings_6_line,
+                    radius: 10,
+                    elevated: false,
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    iconColor: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: const NearByLocationLayout());
   }
 }
--- lib/home/presentation/pages/nearby_location/presentation/widget/nearby_location_layout.dart
+++ lib/home/presentation/pages/nearby_location/presentation/widget/nearby_location_layout.dart
@@ -100,75 +100,19 @@ class NearByLocationLayout extends StatelessWidget {
 
     var size = MediaQuery.of(context).size;
 
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          centerTitle: true,
-          pinned: true,
-          forceElevated: true,
-          leading: const MyRoundIcon.small(
-            icon: Icons.arrow_back,
-            elevated: true,
-          ),
-          title: MyText.h2("NearBy Location"),
-          actions: [
-            Badge(
-              label: MyText.caption(
-                "5",
-                color: Colors.white,
-              ),
-              backgroundColor: Theme.of(context).colorScheme.primary,
-              child: const MyRoundIcon.small(
-                icon: Icons.notifications_none_rounded,
-                elevated: true,
-              ),
-            ),
-          ],
-          bottom: PreferredSize(
-            preferredSize: Size(70, 70),
-            child: Padding(
-              padding: EdgeInsets.only(
-                bottom: 10,
-                left: 16,
-                right: 16,
-              ),
-              child: LayoutBuilder(
-                builder: (context, constraints) => Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: constraints.maxWidth * .88,
-                      child: const MyTextField(
-                        hint: "Search",
-                        prefixIcon: Icon(MingCute.search_2_fill),
-                      ),
-                    ),
-                    MyRoundIcon.small(
-                      icon: MingCute.settings_6_line,
-                      radius: 10,
-                      elevated: false,
-                      backgroundColor: Theme.of(context).colorScheme.primary,
-                      iconColor: Colors.white,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-        SliverGrid(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 16,
-              mainAxisSpacing: 16,
-              childAspectRatio: .79),
-          delegate: SliverChildBuilderDelegate(
-            childCount: properties.length,
-            (context, index) =>
-                MyPropertyCard(properties[index], width: size.width * .5),
-          ),
-        )
-      ],
+    return GridView.builder(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 24,
+      ),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+          childAspectRatio: .68),
+      itemCount: properties.length,
+      itemBuilder: (context, index) =>
+          MyPropertyCard(properties[index], width: size.width * .5),
     );
   }
 }
