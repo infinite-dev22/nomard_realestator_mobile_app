57e0dd3f3b22799c0b6bcec7ed90c0ddc14a2529
infinite-dev22 <mwigojm@gmail.com>
2024-09-05 05:08:03 +0300

    Add search bar and notification icon to Featured page
    This commit adds a search bar and a notification icon to the Featured page. The search bar allows users to search
     for properties, and the notification icon displays the number of unread notifications.

--- lib/home/presentation/pages/featured/presentation/pages/featured.dart
+++ lib/home/presentation/pages/featured/presentation/pages/featured.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:icons_plus/icons_plus.dart';
 
+import '../../../../../../global/presentation/widget/buttons/my_back_button.dart';
+import '../../../../../../global/presentation/widget/icon_holders/my_round_icon.dart';
+import '../../../../../../global/presentation/widget/text_fields/my_text_field.dart';
+import '../../../../../../global/presentation/widget/texts/my_text.dart';
 import '../widget/featured_layout.dart';
 
 class Featured extends StatelessWidget {
@@ -7,6 +13,58 @@ class Featured extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: FeaturedLayout());
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: Builder(
+          builder: (context) => MyBackButton(
+            onTap: () => GoRouter.of(context).pop(),
+          ),
+        ),
+        title: MyText.h2("Featured"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Badge(
+              label: MyText.caption(
+                "5",
+                color: Colors.white,
+              ),
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              child: const MyRoundIcon.small(
+                icon: Icons.notifications_none_rounded,
+                elevated: true,
+              ),
+            ),
+          ),
+        ],
+        bottom: PreferredSize(
+          preferredSize: const Size(double.infinity, 70),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .74,
+                  child: const MyTextField(
+                    hint: "Search",
+                    prefixIcon: Icon(MingCute.search_2_fill),
+                  ),
+                ),
+                MyRoundIcon.small(
+                  icon: MingCute.settings_6_line,
+                  radius: 10,
+                  elevated: false,
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  iconColor: Colors.white,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: FeaturedLayout(),
+    );
   }
 }
