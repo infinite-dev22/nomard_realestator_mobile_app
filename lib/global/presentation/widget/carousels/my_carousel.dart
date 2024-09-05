b7fea26f16d5c5af64d5d37bb688da795c5b224a
infinite-dev22 <mwigojm@gmail.com>
2024-09-05 04:46:22 +0300

    Add MyCarousel widget
    This commit introduces a new widget called MyCarousel, which provides a carousel-style layout for displaying a list of child widgets.
    
    The MyCarousel widget supports horizontal scrolling and allows for customization of the viewport fraction to control the amount of each child widget that is visible. It also includes a snapping behavior to ensure that child widgets are aligned to the center of the viewport when scrolling stops.
    
    This new widget provides a convenient and flexible way to implement carousel
    -style layouts in Flutter applications.

---
+++ lib/global/presentation/widget/carousels/my_carousel.dart
@@ -0,0 +1,148 @@
+import 'dart:math';
+
+import 'package:expandable_page_view/expandable_page_view.dart';
+import 'package:flutter/material.dart';
+
+class MyCarousel extends StatelessWidget {
+  final List<Widget> children;
+  final double viewPortFraction;
+
+  const MyCarousel({
+    super.key,
+    required this.children,
+    this.viewPortFraction = 0.74,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: ExpandablePageView(
+            controller: PageController(viewportFraction: viewPortFraction),
+            padEnds: false,
+            children: children,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MyCarousel2 extends StatelessWidget {
+  // final List<Widget> children;
+  // final double viewPortFraction;
+  final int itemCount;
+  final Widget? Function(BuildContext context, int index) itemBuilder;
+  final double padding;
+  final double width;
+
+  const MyCarousel2({
+    super.key,
+    required this.itemCount,
+    required this.itemBuilder,
+    required this.padding,
+    required this.width,
+    // required this.children,
+    // this.viewPortFraction = 0.74,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      physics: _SnapPageScrollPhysics(
+        elementPadding: padding,
+        elementWidth: width,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: padding),
+      scrollDirection: Axis.horizontal,
+      itemBuilder: itemBuilder,
+      itemCount: itemCount,
+      separatorBuilder: (context, index) => SizedBox(width: padding),
+    );
+  }
+}
+
+class _SnapPageScrollPhysics extends ScrollPhysics {
+  const _SnapPageScrollPhysics({
+    super.parent,
+    required this.elementWidth,
+    required this.elementPadding,
+  });
+
+  final double elementWidth;
+  final double elementPadding;
+
+  @override
+  _SnapPageScrollPhysics applyTo(ScrollPhysics? ancestor) {
+    return _SnapPageScrollPhysics(
+      parent: buildParent(ancestor),
+      elementWidth: elementWidth,
+      elementPadding: elementPadding,
+    );
+  }
+
+  double _getTargetPixels(
+    ScrollMetrics position,
+    Tolerance tolerance,
+    double velocity,
+  ) {
+    final pageWidth = elementWidth + elementPadding;
+    final page = position.pixels / pageWidth + velocity / 3000;
+    final offset = (position.viewportDimension - elementWidth) / 2;
+    final target = page.roundToDouble() * pageWidth - offset + elementPadding;
+    return max(0, min(target, position.maxScrollExtent));
+  }
+
+  @override
+  Simulation? createBallisticSimulation(
+    ScrollMetrics position,
+    double velocity,
+  ) {
+    // If we're out of range and not headed back in range, defer to the parent
+    // ballistics, which should put us back in range at a page boundary.
+    if ((velocity <= 0.0 && position.pixels <= position.minScrollExtent) ||
+        (velocity >= 0.0 && position.pixels >= position.maxScrollExtent)) {
+      return super.createBallisticSimulation(position, velocity);
+    }
+    final tolerance = toleranceFor(position);
+    final target = _getTargetPixels(position, tolerance, velocity);
+    if (target != position.pixels) {
+      return ScrollSpringSimulation(
+        spring,
+        position.pixels,
+        target,
+        velocity,
+        tolerance: tolerance,
+      );
+    }
+    return null;
+  }
+
+  @override
+  bool get allowImplicitScrolling => false;
+}
+
+class MyCarousel3 extends StatelessWidget {
+  final double viewPortFraction;
+  final int itemCount;
+  final Widget? Function(BuildContext context, int index) itemBuilder;
+
+  const MyCarousel3({
+    super.key,
+    required this.itemCount,
+    required this.itemBuilder,
+    this.viewPortFraction = 0.74,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      controller: PageController(viewportFraction: viewPortFraction),
+      itemCount: itemCount,
+      itemBuilder: itemBuilder,
+      padEnds: false,
+    );
+  }
+}
