1fd309688fd5bb5222b1fd040d2d16173dc63a85
infinite-dev22 <mwigojm@gmail.com>
2024-09-05 04:46:54 +0300

    Add elevation to PropertyCard
    Added elevation to the PropertyCard widget to enhance its visual appearance and make it stand out.

--- lib/global/presentation/widget/property_card/my_property_card.dart
+++ lib/global/presentation/widget/property_card/my_property_card.dart
@@ -20,37 +20,32 @@ class MyPropertyCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 350),
-      width: width ?? double.infinity,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Theme.of(context).colorScheme.surface,
-        boxShadow: [
-          BoxShadow(
-            color: Theme.of(context).colorScheme.shadow.withOpacity(.1),
-            spreadRadius: 1,
-            blurRadius: .1,
-            offset: const Offset(0, 1), // changes position of shadow
-          ),
-        ],
-      ),
-      child: Column(
-        children: [
-          BlocProvider(
-            create: (context) => PropertyCardBloc(),
-            child: PropertyCardImage(
-              propertyModel.images[1],
-              width: double.infinity,
-              height: height,
+    return Material(
+      elevation: 16,shadowColor: Theme.of(context).colorScheme.shadow,borderRadius: BorderRadius.circular(8),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 350),
+        width: width ?? double.infinity,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Theme.of(context).colorScheme.surface,
+        ),
+        child: Column(
+          children: [
+            BlocProvider(
+              create: (context) => PropertyCardBloc(),
+              child: PropertyCardImage(
+                propertyModel.images[1],
+                width: double.infinity,
+                height: height,
+              ),
             ),
-          ),
-          PropertyCardDetails(
-            propertyModel,
-            large: width == null,
-          ),
-        ],
+            PropertyCardDetails(
+              propertyModel,
+              large: width == null,
+            ),
+          ],
+        ),
       ),
     );
   }
